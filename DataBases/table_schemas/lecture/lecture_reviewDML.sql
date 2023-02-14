d9c962aa4f32497fc850ae46fffd1e48f87f0735
chanwho <anxi0015@gmail.com>
2023-02-14 15:01:44 +0900

    add DML

---
+++ DataBases/table_schemas/lecture/lecture_reviewDML.sql
@@ -0,0 +1,5 @@
+insert into lecture_review (SEARCH_WORD, POST_NUMBER, POST_TITLE, POST_REVIEW_COUNT, POSTING_USER, POSTING_DATE, VIEW_COUNT, PAGE_NUMBER) 
+values (null, 1, 'abc', 1, 'mike', '2022-02-14', 15, 1);
+
+insert into lecture_review (SEARCH_WORD, POST_NUMBER, POST_TITLE, POST_REVIEW_COUNT, POSTING_USER, POSTING_DATE, VIEW_COUNT, PAGE_NUMBER) 
+values (null, 2, 'bbb', 2, 'shawn', '2022-02-13', 12, 2);
