986a2f849306c5fa254f42a2eb19e6385fdaa404
wonki eun <eun30137@naver.com>
2023-02-16 11:08:59 +0900

    -

--- DataBases/table_schemas/community/community_postDML.sql
+++ DataBases/table_schemas/community/community_postDML.sql
@@ -1,5 +1,5 @@
 insert into community_post 
-(COMMUNITY_NUMBER,COMMUNITY_CONTENT,COMMUNITY_COMMENT,COMMUNITY_TITLE,FILE_ATTACHIMENT) 
+(POST_NUMBER,COMMUNITY_NUMBER,COMMUNITY_CONTENT,COMMUNITY_COMMENT,COMMUNITY_TITLE,FILE_ATTACHIMENT) 
 values 
-('c3', 'good lecture', 'good job', 'Thank you', 'Y'), 
-('c3', 'nice lecture', 'nice', 'Im happy', 'N');
+('p13','c3', 'good lecture', 'good job', 'Thank you', 'Y'), 
+('p44','c3', 'nice lecture', 'nice', 'Im happy', 'N');
