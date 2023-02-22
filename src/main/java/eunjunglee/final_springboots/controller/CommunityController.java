3d2928dd871bff00d333d2f0f491b5563c158f13
jhun1020 <jhun1020.kh@gmail.com>
2023-02-22 11:54:59 +0900

    ㅁㄴㅇㄹ

--- src/main/java/eunjunglee/final_springboots/controller/CommunityController.java
+++ src/main/java/eunjunglee/final_springboots/controller/CommunityController.java
@@ -2,15 +2,20 @@ package eunjunglee.final_springboots.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import eunjunglee.final_springboots.service.CommunityService;
+
 @Controller
 public class CommunityController {
 
+    @Autowired
+    CommunityService communityService;
  
     
     // 강좌게시판 ㅇ
@@ -24,18 +29,18 @@ public class CommunityController {
 
     // 강사게시판 ㅇ
     @RequestMapping(value="/community_lecturer", method = RequestMethod.GET)
-    public ModelAndView getLecturer(ModelAndView modelAndView ){
-        // Object resultMap = 서비스.get서비스메소드(params);
-        // modelAndView.addObject("resultMap", resultMap);
+    public ModelAndView getLecturer(ModelAndView modelAndView, @RequestParam Map<String, Object> params ){
+        Object resultMap = communityService.getList(params);
+        modelAndView.addObject("resultMap", resultMap);
         modelAndView.setViewName("/communitys/community_lecturer");
         return modelAndView;
     }
 
     //공지사항 게시판 ㅇ
     @RequestMapping(value="/community_notice", method = RequestMethod.GET)
-    public ModelAndView getNotice(ModelAndView modelAndView ){
-        // Object resultMap = 서비스.get서비스메소드(params);
-        // modelAndView.addObject("resultMap", resultMap);
+    public ModelAndView getNotice(ModelAndView modelAndView, @RequestParam Map<String, Object> params ){
+        Object resultMap = communityService.getList(params);
+        modelAndView.addObject("resultMap", resultMap);
         modelAndView.setViewName("/communitys/community_notice");
         return modelAndView;
     }
@@ -60,9 +65,9 @@ public class CommunityController {
 
     // 학생게시판 ㅇ
     @RequestMapping(value="/community_student", method = RequestMethod.GET)
-    public ModelAndView getStudent( ModelAndView modelAndView ){
-        // Object resultMap = 서비스.get서비스메소드(params);
-        // modelAndView.addObject("resultMap", resultMap);
+    public ModelAndView getStudent( ModelAndView modelAndView, @RequestParam Map<String, Object> params ){
+        Object resultMap = communityService.getList(params);
+        modelAndView.addObject("resultMap", resultMap);
         modelAndView.setViewName("/communitys/community_student");
         return modelAndView;
     }
